7358a67154ac7d2530487d925587df483fbdfb3c
Prabhat <osrprabhatyadav4@gmail.com>
2026-08-16 01:38:36 +0530

    Add TypeScript inference and type annotations example

---
+++ src/02_typesInTS.ts
@@ -0,0 +1,13 @@
+// Inference in Typescript
+
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : '5';
+
+// Type Annotations
+
+let chaiFlavour: string = "Masala Chai";
+
+chaiFlavour = "Ginger Tea"; // ✅
+
+//chaiFlavour = 21   // ❌  
